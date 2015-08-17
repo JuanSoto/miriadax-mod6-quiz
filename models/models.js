2df651e4efc0b9c61694df3c1ecef2a186e25327
Juan Soto <juan.soto@edify.cr>
2015-08-17 17:45:03 -0600

    se agrega pg-hstore

--- models/models.js
+++ models/models.js
@@ -11,9 +11,9 @@ var host = (url[4]||null);
 var storage = (process.env.DATABASE_STORAGE||null);
 
 var Sequelize = require('sequelize');
-// var sequelize = new Sequelize(DB_name, user, pwd, 
-	// {dialect:dialect, protocol:protocol, port:port, host:host, storage:storage, omitNull:true});
-var sequelize = new Sequelize(process.env.DATABASE_URL);
+var sequelize = new Sequelize(DB_name, user, pwd, 
+	{dialect:dialect, protocol:protocol, port:port, host:host, storage:storage, omitNull:true});
+// var sequelize = new Sequelize(process.env.DATABASE_URL, {dialect:dialect, protocol:protocol, port:port, host:host, storage:storage, omitNull:true} );
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
 exports.Quiz = Quiz;
